631db4e628d1149c483e75ac8fb9c1ccc07d602e
cecilyp <cecilyrbpage@gmail.com>
2018-12-03 19:07:00 -0500

    removed mysterious shadow ink

--- ink.cpp
+++ ink.cpp
@@ -6,18 +6,22 @@
 #include "engine/circle.h"
 
 Ink::Ink(position2D::Vector2D point): Entity(point), position(point.x, point.y), inkColor({0, 0, 0}) {
-
+    splatters.emplace_back(8, position, inkColor);
+    for(int i = 0; i < 15; i++){
+        int rad = rand() % 4;
+        position2D::Vector2D edge = splatters[0].getPointOnEdge();
+        splatters.emplace_back(rad, edge, inkColor);
+    }
 }
 
 void Ink::draw() {
-
-    splatters.emplace_back(8, position, inkColor);
     for(int i = 0; i < 15; i++){
         int rad = rand() % 4;
         position2D::Vector2D edge = splatters[0].getPointOnEdge();
         splatters.emplace_back(rad, edge, inkColor);
 
     }
+
 
     for (auto &splatter : splatters) {
         splatter.draw();
